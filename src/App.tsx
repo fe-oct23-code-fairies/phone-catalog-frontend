3af2719c94584366c98c9d76455cdf4cbecc78c4
Maksym Svoboda <maksim4ik71@gmail.com>
2024-01-19 14:05:33 +0200

    testing lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import './App.css';
 
 const App: React.FC = () => {
+  const a = 1;
+  const b = 1;
+
+  if (a === b) {
+    return <></>;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
