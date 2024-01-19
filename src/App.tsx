36c2123d34ce868a9a2fb743735a2e2c57b42d6d
Maksym Svoboda <maksim4ik71@gmail.com>
2024-01-19 14:06:33 +0200

    environment setup finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
 import './App.css';
 
 const App: React.FC = () => {
-  const a = 1;
-  const b = 1;
-
-  if (a === b) {
-    return <></>;
-  }
-
   return (
     <div className="App">
       <header className="App-header">
