79fcb9cbf3e9ffa2704a516425861548ffa8fa1f
Ruslana Gnatenko <rgnatenko5@gmail.com>
2024-01-22 16:18:28 +0300

    add capacity select, add to favourite button, color radio button and circcle button with icon and with text

---
+++ src/ui/CapacitySelect/CapacitySelect.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type Props = {
+  gigabytesAmount: number
+};
+
+export const CapacitySelect: React.FC<Props> = ({ gigabytesAmount }) => {
+  return (
+    <button
+      type="button"
+      className="button-rectangle"
+    >
+      {`${gigabytesAmount} GB`}
+    </button>
+  );
+};
