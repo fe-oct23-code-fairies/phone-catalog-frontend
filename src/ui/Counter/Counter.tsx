178c621381c11ca1a0635370b7d8e11ae182e5b6
Yevhen Kolosov <kolosov214a@gmail.com>
2024-01-23 09:03:23 +0200

    fixed positioning of CartItem items and counter

--- src/ui/Counter/Counter.tsx
+++ src/ui/Counter/Counter.tsx
@@ -3,7 +3,7 @@ import { Icon } from '../Icons';
 import { CircleButtonWithIcon } from '../CircleButtonWithIcon';
 
 export const Counter: React.FC = () => {
-  const MAX_VALUE = 5;
+  const MAX_VALUE = 12;
 
   const [counter, setCounter] = useState(1);
   const isMinusDisabled = counter === 1;
@@ -18,7 +18,7 @@ export const Counter: React.FC = () => {
         <Icon iconName="minus" />
       </CircleButtonWithIcon>
 
-      <p className="text-body">{counter}</p>
+      <p className="text-body counter__number">{counter}</p>
 
       <CircleButtonWithIcon disabled={isPlusDisabled} onClick={onClickPlus}>
         <Icon iconName="plus" />
