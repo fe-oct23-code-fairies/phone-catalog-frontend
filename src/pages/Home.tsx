aeb5ec2855c9b15c0f5a2db31979fdf6a2a684ff
olegnovikov10 <relaxinfh071@gmail.com>
2024-01-24 22:19:47 +0200

    finish sliders phones

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
-// import { PhotoBlock } from '../components/photoBlock';
+import { PhotoBlock } from '../components/photoBlock';
 import { MainSlider } from '../components/MainSlider/MainSlider';
 import { PhonesSection } from '../components/PhonesSection/Phones';
 
 export const Home: React.FC = () => {
   return (
     <>
-      { /* <PhotoBlock /> */}
+      <PhotoBlock />
       <MainSlider />
       <PhonesSection prefixSlider="models" title="Brand new model" />
       <PhonesSection prefixSlider="hot" title="Hot prices" />
