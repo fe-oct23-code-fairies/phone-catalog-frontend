1d96b101083631ccd7d6f005f69b4602aace4ad0
Oleksii Mantserov <a.v.mantserov@gmail.com>
2024-01-25 12:35:36 +0200

    Added loader

--- src/pages/Phones/Phones.tsx
+++ src/pages/Phones/Phones.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { CardLayout } from '../../components/CardLayout';
 import { client } from '../../utils/fetchClient';
 import { Product } from '../../types/Product';
+import { Loader } from '../../components/Loader/Loader';
 
 export const Phones: React.FC = () => {
   const [toShow, setToShow] = useState<Product[]>([]);
@@ -18,26 +19,25 @@ export const Phones: React.FC = () => {
       });
   }, []);
 
-  if (isLoading) {
-    return <h3>Loading...</h3>; // It is temporary solution, I think it will be good to add special loader component.
-  }
-
   return (
     <>
       <h1 className="phones__title">Mobile phones</h1>
-
-      <div className="grid__container">
-        {toShow.map((phone) => (
-          <CardLayout
-            key={phone.id}
-            phoneName={phone.name}
-            phonePrice={phone.price}
-            phoneScreen={phone.screen}
-            phoneCapacity={phone.capacity}
-            phoneRam={phone.ram}
-          />
-        ))}
-      </div>
+      {isLoading
+        ? <Loader />
+        : (
+          <div className="grid__container">
+            {toShow.map((phone) => (
+              <CardLayout
+                key={phone.id}
+                phoneName={phone.name}
+                phonePrice={phone.price}
+                phoneScreen={phone.screen}
+                phoneCapacity={phone.capacity}
+                phoneRam={phone.ram}
+              />
+            ))}
+          </div>
+        )}
     </>
   );
 };
