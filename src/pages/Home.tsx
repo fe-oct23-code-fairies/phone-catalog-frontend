445578766bc389962247999e11fc6c2d457e1c7a
olegnovikov10 <relaxinfh071@gmail.com>
2024-01-26 14:48:17 +0200

    fix -problems

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-// import { PhotoBlock } from '../components/photoBlock';
+import { PhotoBlock } from '../components/photoBlock';
 import { MainSlider } from '../components/MainSlider/MainSlider';
 import { PhonesSection } from '../components/PhonesSection/PhoneSection';
 import { Breadcrumbs } from '../components/Breadcrumbs/Breadcrumbs';
@@ -7,7 +7,7 @@ export const Home: React.FC = () => {
   return (
     <>
       <Breadcrumbs />
-      { /* <PhotoBlock /> */}
+      <PhotoBlock />
       <MainSlider />
       <PhonesSection prefixSlider="models" title="Brand new model" />
       <PhonesSection prefixSlider="hot" title="Hot prices" />
